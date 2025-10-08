81a7a39651e1ab885a813f57ad9da5e24dd6b4b9
nothier7 <thiernolad@gmail.com>
2025-10-07 21:00:55 -0400

    feat(agent): update random.py

--- random.py
+++ random.py
@@ -1,4 +1,20 @@
 import random
 
+
+ASCII_ART = r"""
+  ____                 _                 _              
+ |  _ \ __ _ _ __ ___ (_) ___  _ __ ___ | |__   ___ _ __ 
+ | |_) / _` | '_ ` _ \| |/ _ \| '_ ` _ \| '_ \ / _ \ '__|
+ |  _ < (_| | | | | | | | (_) | | | | | | |_) |  __/ |   
+ |_| \_\__,_|_| |_| |_|_|\___/|_| |_| |_|_.__/ \___|_|   
+"""
+
+
+def main():
+    number = random.randint(1, 100)
+    print(ASCII_ART)
+    print(f"Your random number between 1 and 100 is: {number}")
+
+
 if __name__ == "__main__":
-    print(random.randint(1, 100))
+    main()
